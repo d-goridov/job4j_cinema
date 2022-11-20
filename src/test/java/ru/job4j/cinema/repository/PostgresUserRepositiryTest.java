4ccab07dd5cfb50b500d10524433231060f7dd88
d.goridov <dimagoridov@yandex.ru>
2022-11-20 19:28:56 +0300

    Настройки профилей, файлы настроек liquibase и H2. Пробный тест.

---
+++ src/test/java/ru/job4j/cinema/repository/PostgresUserRepositiryTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.cinema.repository;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.cinema.Main;
+import ru.job4j.cinema.model.User;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PostgresUserRepositiryTest {
+
+    @Test
+    public void whenAddUser() {
+        UserRepository repository = new PostgresUserRepositiry(new Main().loadPool());
+        User user = new User(1, "Tom", "tom123@gmail.com", "222-33-44");
+        Optional<User> userDB = repository.add(user);
+        assertThat(userDB.isPresent()).isTrue();
+        User inDb = userDB.get();
+        assertThat(inDb.getName()).isEqualTo(user.getName());
+    }
+
+    @Test
+    public void whenFindByEmailAndPhone() {
+        UserRepository repository = new PostgresUserRepositiry(new Main().loadPool());
+        User user = new User(2, "Oleg", "oleg123@gmail.com", "222-55-44");
+        Optional<User> userDB = repository.add(user);
+        assertThat(userDB.isPresent()).isTrue();
+        Optional<User> inDB = repository.findUserByEmailAndPhone(user.getEmail(), user.getPhone());
+        assertThat(inDB.isPresent()).isTrue();
+        User userInDB = inDB.get();
+        assertThat(user.getPhone()).isEqualTo(userInDB.getPhone());
+        assertThat(user.getEmail()).isEqualTo(userInDB.getEmail());
+
+    }
+}
